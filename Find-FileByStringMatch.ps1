e9c9d6434c628f36e37fe306799e96bba9e16352
zbarton <31739099+zbarton@users.noreply.github.com>
2021-05-24 11:53:11 -0400

    Created new find script with optional deletion

---
+++ Find-FileByStringMatch.ps1
@@ -0,0 +1,36 @@
+﻿function Find-FileByStringMatch{
+    param(
+        [Parameter(Mandatory=$true,Position=0)]
+        [string]
+        $matchString
+        ,[string]
+        $directory = ($pwd.Path)
+        ,[string]
+        $logPath = "$($pwd.Path)\FileLog$(Get-Date -Format yyyy-MM-dd_hh-mm).txt"
+        ,[switch]
+        $removeFiles
+    )
+
+    Process{
+        #Write-Host "Operating on directory $directory"
+        $itemList = Get-ChildItem $directory -Recurse
+
+        foreach($item in $itemList){
+            if(Test-Path $item.FullName -PathType Leaf){
+                #Write-Host "Matched File for $($item.FullName), testing"
+                if ($item.FullName -match $matchString){
+                    Out-File -InputObject "Found $($item.FullName)" -FilePath $logPath -Append
+                    try{
+                        if($removeFiles){
+                            Out-File -InputObject "Removal enabled, removing $($item.FullName)" -FilePath $logPath -Append
+                            Remove-Item -Path $item.FullName -Force
+                        }
+                    }
+                    catch{
+                        Out-File -InputObject "UNABLE TO REMOVE $($item.FullName)" -FilePath $logPath -Append
+                    }
+                }
+            }
+        }
+    }
+}
