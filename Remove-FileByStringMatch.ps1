fd37742d08e5547707e31e22aff1b784adf5ea8f
zbarton <31739099+zbarton@users.noreply.github.com>
2021-05-24 10:10:47 -0400

    Created File Removal Script

---
+++ Remove-FileByStringMatch.ps1
@@ -0,0 +1,32 @@
+﻿function Remove-FileByStringMatch{
+    param(
+        [Parameter(ValueFromPipeline=$true)]
+        [string]
+        $directory = ($pwd.Path)
+        ,[string]
+        $logPath = "$($pwd.Path)\RemovedFileLog$(Get-Date -Format yyyy-MM-dd-hh:mm).txt"
+        ,[string]
+        $matchString = ".eking"
+    )
+
+    Process{
+        #Write-Host "Operating on directory $directory"
+        $itemList = Get-ChildItem $filePath -Recurse
+
+        foreach($item in $itemList){
+            if(Test-Path $item.FullName -PathType Leaf){
+                #Write-Host "Matched File for $($item.FullName), testing"
+                if ($item.FullName -match $matchString){
+                    try{
+                        Remove-Item -Path $item.FullName -Force
+                        Out-File -InputObject "Found and removed $($item.FullName)" -FilePath $logPath -Append
+                        }
+                    catch{
+                        Out-File -InputObject "UNABLE TO REMOVE $($item.FullName)" -FilePath $logPath -Append
+                    }
+                }
+            }
+        }
+    
+    }
+}
